3fc51c74a5d23d0784c4a4c5627ebdbbffb638e8
Wade Lieurance <wlieurance@gmail.com>
2019-08-23 14:23:57 -0700

    initial commit

---
+++ other_views/ecogroup_plantprod_delimitted.sql
@@ -0,0 +1,20 @@
+--creates a comma delimitted list of plants in an ecogroup in descending production order
+SELECT a.ecogroup, coalesce(b.production, c.production) AS production
+  FROM (SELECT ecogroup FROM soil.ecogroup_plantprod GROUP BY ecogroup) AS a
+  LEFT JOIN (
+		SELECT ecogroup, string_agg(plantsym, ',') AS production
+		  FROM (
+			SELECT ecogroup, plantsym, prodtype, prod
+				FROM soil.ecogroup_plantprod
+				WHERE prodtype = 'range'
+				ORDER BY ecogroup, prod DESC, plantsym) AS a
+		 GROUP BY ecogroup, prodtype) AS b ON a.ecogroup = b.ecogroup
+  LEFT JOIN (
+  		SELECT ecogroup, string_agg(plantsym, ',') AS production
+		  FROM (
+			SELECT ecogroup, plantsym, prodtype, prod
+				FROM soil.ecogroup_plantprod
+				WHERE prodtype LIKE 'forest%'
+				ORDER BY ecogroup, prod DESC, plantsym) AS a
+		 GROUP BY ecogroup, prodtype) AS c ON a.ecogroup = c.ecogroup
+ ORDER BY ecogroup;
